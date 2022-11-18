ad625673da9a23a9fe42e5e4e5011db52ddbb523
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2022-11-18 17:10:28 +0530

    Project structure created

---
+++ ShopEasy/src/main/java/com/shopeasy/ShopEasyApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopEasyApplication.class, args);
+	}
+
+}
---
+++ ShopEasy/src/test/java/com/shopeasy/ShopEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
